a850d5170ea50caac094dda8734589821f481d7a
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2021-11-26 09:24:03 -0600

    .

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,19 @@
-//  Add your code here
+//1. IMPORTACIONES
+const mongoose = require("mongoose");
+
+//2.SCHEMA
+const celebritySchema = mongoose.Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+  },
+  {
+    timestamps: true, //Establecer la fecha en la cual fue creado
+  }
+);
+//3. MODELO
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+//4. EXPORTACION
+module.exports = Celebrity;
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,13 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+//1. IMPORTACIONES
+const express = require("express");
+const router = require("express").Router();
+const celebrityController = require("./../controllers/celebrityController");
+
+//2. RUTEO CON BASE URL
+
+//3.RUTAS
+
+//4. EXPORTACION
+
+module.exports = router;
